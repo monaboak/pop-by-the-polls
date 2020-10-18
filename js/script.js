678b1b34784d65b0180c7b5779d23b46e65e778d
monaboak <71105974+monaboak@users.noreply.github.com>
2020-10-18 14:43:19 -0400

    Nav Javascript

--- js/script.js
+++ js/script.js
@@ -1,15 +1,27 @@
 /* Navbar */
 
 // get the hamburger element
+var hambuger = document.getElementById("hamburger");
 
 // get the menu element
+var menu = document.getElementById("menu");
 
 // get the overlay element
+var menu = document.getElementById("overlay");
 
 // define toggle function
+// either adds or removes active class from element
+function toggleMenu() {
+  menu.classList.toggle("active");
+  overlay.classList.toggle("active");
+}
 
 // execute toggle function from hamburger on click
+hamburger.addEventListener("click", toggleMenu);
+// execute toggle function from overlay on click
+overlay.addEventListener("click", toggleMenu);
 
-// exuecute toggle function from overlay on click
+// execute toggle function from menu on click
+menu.addEventListener("click", toggleMenu);
 
 /* Navbar */
